4f86f29d3f5db1f42107ce71cb06821206c7e8b5
ramiKAREMLOL <53526543+ramiKAREMLOL@users.noreply.github.com>
2019-07-31 16:43:34 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("606115375394062346")
+setInterval(function() {
+channel.send(`كريمهلواتزميا`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
